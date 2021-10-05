e1b8aadf90d0b7549a99fbf8b781e3384dd3627d
Redd Caruana <redd.caruana@mcast.edu.mt>
2021-10-05 11:14:03 +0200

    Lecture 1
    Added files for the first lecture to the root website.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LectureComponent } from './lecture/lecture.component';
+import { DirectoryComponent } from './directory/directory.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LectureComponent
+    LectureComponent,
+    DirectoryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
